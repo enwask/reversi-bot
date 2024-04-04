6850c0287b16f7bbefaf6940165c09792071286a
enwask <enwask@ucf.edu>
2024-04-04 16:46:34 -0400

    Test with configurable color assignment

--- framework/reversi.c
+++ framework/reversi.c
@@ -35,38 +35,38 @@ int main(void) {
 }
 
 void humanVHuman() {
-
+    
     // Initialize and print the board. Black goes first.
     enum piece board[SIZE][SIZE];
     initBoard(board);
     printBoard(board);
     enum piece player = BLACK;
-
+    
     // Go till the game is done.
     while (!gameOver(board)) {
-
+        
         // We can flip the team if the current team can't move.
         if (!canMove(board, player))
             player = opposite(player);
-
+        
         // Get this player's move and execute it.
         position *mymove = getUserMove(board, player);
         executeMove(board, mymove, player);
         free(mymove);
-
+        
         // Print the result and go to the other player.
         printBoard(board);
         player = opposite(player);
     }
-
+    
     // Get scores.
     int blackScore = score(board, BLACK);
     int whiteScore = score(board, WHITE);
-
+    
     // Print scores.
     printf("Black's score is %d\n", blackScore);
     printf("White's score is %d\n", whiteScore);
-
+    
     // Output the result.
     if (blackScore > whiteScore)
         printf("Black wins by %d points.\n", blackScore - whiteScore);
@@ -77,90 +77,90 @@ void humanVHuman() {
 }
 
 void computerVComputer() {
-
+    
     // Initialize and print the board. Black goes first.
     enum piece board[SIZE][SIZE];
     initBoard(board);
     printBoard(board);
     enum piece player = BLACK;
-
+    
     int blackTime = MAXTIME;
     int whiteTime = MAXTIME;
     enum boolean blackOutOfTime = FALSE;
     enum boolean whiteOutOfTime = FALSE;
-
+    
     // Go till the game is done.
     while (!gameOver(board)) {
-
+        
         // We can flip the team if the current team can't move.
         if (!canMove(board, player))
             player = opposite(player);
-
+        
         position *mymove = NULL;
-
+        
         // Black Computer Player
         if (player == BLACK) {
-
+            
             // Do the move and time it.
             int startT = time(0);
             mymove = team03Move(board, player, blackTime);
             int endT = time(0);
-
+            
             // Update time.
             blackTime = blackTime - (endT - startT);
             printf("The black team selected row %d, column %d\n", mymove->x, mymove->y);
         }
-
+            
             // White Computer Player
         else {
-
+            
             // Do the move and time it.
             int startT = time(0);
             mymove = team21Move(board, player, whiteTime);
             int endT = time(0);
-
+            
             // Update time.
             whiteTime = whiteTime - (endT - startT);
             printf("The white team selected row %d, column %d\n", mymove->x, mymove->y);
         }
-
+        
         // Execute then free move.
         executeMove(board, mymove, player);
         free(mymove);
-
+        
         // Black ran out of time.
         if (blackTime < 0) {
             printf("Sorry, the Black Player ran out of time and loses automatically.\n");
             blackOutOfTime = TRUE;
             break;
         }
-
+        
         // White ran out of time.
         if (whiteTime < 0) {
             printf("Sorry, the White Player ran out of time and loses automatically.\n");
             whiteOutOfTime = TRUE;
             break;
         }
-
+        
         // Print the result and go to the other player.
         printBoard(board);
         player = opposite(player);
-
+        
         // This waits about one second, so we can see the moves scrolling.
         //int curT = time(0);
         //while (time(0) == curT);
     }
-
+    
     if (!blackOutOfTime && !whiteOutOfTime) {
-
+        
         // Get scores.
         int blackScore = score(board, BLACK);
         int whiteScore = score(board, WHITE);
-
+        
         // Print scores.
         printf("Black's score is %d\n", blackScore);
         printf("White's score is %d\n", whiteScore);
-
+        
         // Output the result.
         if (blackScore > whiteScore)
             printf("Black wins by %d points.\n", blackScore - whiteScore);
@@ -169,7 +169,7 @@ void computerVComputer() {
         else
             printf("It's a tie!\n");
     }
-
+        
         // Black player ran out of time.
     else if (blackOutOfTime) {
         printf("White wins because the Black Player ran out of time.\n");
@@ -181,68 +181,68 @@ void computerVComputer() {
 }
 
 void humanVComputer() {
-
+    
     // Initialize and print the board. Black goes first.
     enum piece board[SIZE][SIZE];
     initBoard(board);
     printBoard(board);
     enum piece player = BLACK;
-
+    
     int compTime = MAXTIME;
     enum boolean outOfTime = FALSE;
-
+    
     // Go till the game is done.
     while (!gameOver(board)) {
-
+        
         // We can flip the team if the current team can't move.
         if (!canMove(board, player))
             player = opposite(player);
-
+        
         position *mymove = NULL;
-
+        
         // Hard-coded human first.
         if (player == BLACK)
             mymove = getUserMove(board, player);
-
+            
             // Computer Second.
         else {
-
+            
             // Do the move and time it.
             int startT = time(0);
             mymove = team03Move(board, player, compTime);
             int endT = time(0);
-
+            
             // Update time.
             compTime = compTime - (endT - startT);
             printf("The computer selected row %d, column %d\n", mymove->x, mymove->y);
         }
-
+        
         // Execute then free move.
         executeMove(board, mymove, player);
         free(mymove);
-
+        
         // Time issue.
         if (compTime < 0) {
             printf("Sorry, the computer ran out of time and loses automatically.\n");
             outOfTime = TRUE;
             break;
         }
-
+        
         // Print the result and go to the other player.
         printBoard(board);
         player = opposite(player);
     }
-
+    
     if (!outOfTime) {
-
+        
         // Get scores.
         int blackScore = score(board, BLACK);
         int whiteScore = score(board, WHITE);
-
+        
         // Print scores.
         printf("Black's score is %d\n", blackScore);
         printf("White's score is %d\n", whiteScore);
-
+        
         // Output the result.
         if (blackScore > whiteScore)
             printf("Black wins by %d points.\n", blackScore - whiteScore);
@@ -251,116 +251,111 @@ void humanVComputer() {
         else
             printf("It's a tie!\n");
     }
-
+        
         // Ran out of time.
     else {
         printf("Computer has defaulted and lost.\n");
     }
 }
 
-
-
-
-int play() {
-
+int play(enum piece ourColor) {
+    
     // Initialize and print the board. Black goes first.
     enum piece board[SIZE][SIZE];
     initBoard(board);
     //printBoard(board);
     enum piece player = BLACK;
-
+    
     int blackTime = MAXTIME;
     int whiteTime = MAXTIME;
     enum boolean blackOutOfTime = FALSE;
     enum boolean whiteOutOfTime = FALSE;
-
+    
     // Go till the game is done.
     while (!gameOver(board)) {
-
+        
         // We can flip the team if the current team can't move.
         if (!canMove(board, player))
             player = opposite(player);
-
+        
         position *mymove = NULL;
-
+        
         // Black Computer Player
-        if (player == BLACK) {
-
+        if (player == ourColor) {
+            
             // Do the move and time it.
             int startT = time(0);
             mymove = team03Move(board, player, blackTime);
             int endT = time(0);
-
+            
             // Update time.
             blackTime = blackTime - (endT - startT);
             //printf("The black team selected row %d, column %d\n", mymove->x, mymove->y);
         }
-
+            
             // White Computer Player
         else {
-
+            
             // Do the move and time it.
             int startT = time(0);
             mymove = teamrandMove(board, player, whiteTime);
             int endT = time(0);
-
+            
             // Update time.
             whiteTime = whiteTime - (endT - startT);
             //printf("The white team selected row %d, column %d\n", mymove->x, mymove->y);
         }
-
+        
         // Execute then free move.
         executeMove(board, mymove, player);
         free(mymove);
-
+        
         // Black ran out of time.
         if (blackTime < 0) {
             //printf("Sorry, the Black Player ran out of time and loses automatically.\n");
             blackOutOfTime = TRUE;
             break;
         }
-
+        
         // White ran out of time.
         if (whiteTime < 0) {
             //printf("Sorry, the White Player ran out of time and loses automatically.\n");
             whiteOutOfTime = TRUE;
             break;
         }
-
+        
         // Print the result and go to the other player.
         //printBoard(board);
         player = opposite(player);
-
+        
         // This waits about one second, so we can see the moves scrolling.
         //int curT = time(0);
         //while (time(0) == curT);
     }
-
+    
     if (!blackOutOfTime && !whiteOutOfTime) {
-
+        
         // Get scores.
         int blackScore = score(board, BLACK);
         int whiteScore = score(board, WHITE);
-
+        
         // Print scores.
         //printf("Black's score is %d\n", blackScore);
         //printf("White's score is %d\n", whiteScore);
-
+        
         // Output the result.
         if (blackScore > whiteScore) {
             //printf("Black wins by %d points.\n", blackScore - whiteScore);
             return 2;
-        }
-        else if (whiteScore > blackScore) {
+        } else if (whiteScore > blackScore) {
             //printf("White wins by %d points.\n", whiteScore - blackScore);
             return 0;
-        }
-        else {
+        } else {
             //printf("It's a tie!\n");
             return 1;
         }
     }
-
+        
         // Black player ran out of time.
     else if (blackOutOfTime) {
         //printf("White wins because the Black Player ran out of time.\n");
@@ -394,12 +389,21 @@ int play() {
 
 void test() {
     srand(time(NULL));
-    int black = 0; int white = 0;
-    for (int i = 0; i < 100; i++) {
-        int res = play();
-        black += res;
-        white += 2 - res;
-        printf("%d\n", i);
+    int us = 0;
+    int them = 0;
+    int tie = 0;
+//    int black = 0;
+//    int white = 0;
+    for (int i = 1; i <= 100; i++) {
+        enum piece ourColor = (i % 2) ? WHITE : BLACK;
+        int res = play(ourColor);
+        if (res == 1) tie++;
+        else {
+            enum piece winColor = (res == 0) ? WHITE : BLACK;
+            if (winColor == ourColor) us++;
+            else them++;
+        }
+        if (i % 10 == 0) printf("%d\n", i);
     }
-    printf("SMARTER: %d\nRANDOM: %d\n", black, white);
+    printf("us (team03): %d\nthem (rand): %d\ntied: %d\n", us, them, tie);
 }
--- framework/reversi.h
+++ framework/reversi.h
@@ -1,12 +1,13 @@
 #ifndef REVERSI_H_INCLUDED
 #define REVERSI_H_INCLUDED
 
+#include "reversi_functions.h"
 #define MAXTIME 40
 
 void humanVHuman();
 void humanVComputer();
 void computerVComputer();
-int play();
+int play(enum piece ourColor);
 void test();
 
 #endif // REVERSI_H_INCLUDED
